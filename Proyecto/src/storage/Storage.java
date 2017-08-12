e9d9ab0476fcad9ed84916bcfd960b4a519f47a9
Sergio Sanchez <sergioj.sanchezr@gmail.com>
2017-08-12 00:17:29 -0500

    Agregadas un par de clases nuevas

---
+++ Proyecto/src/storage/Storage.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package storage;
+
+import java.io.*;
+/**
+ *
+ * @author Sergio
+ */
+public class Storage {
+    
+    private String rootFolderName;
+    
+    public Storage(String rootFolderName){
+        this.rootFolderName = rootFolderName;
+    }
+}
